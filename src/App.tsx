7207e9cc662a5357a17978b1d54f5238c61338d9
Sach <1822416.bca.cbsa@cgc.edu.in>
2022-02-21 16:55:50 +0530

    Navbar Finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
-import { Divider } from '@chakra-ui/react';
 import './App.css';
 import Banner from './Componenets/Banner';
 import Navbar from './Componenets/Navbar';
-import SectionInfo from './Componenets/SectionInfo';
 
 function App() {
   return (
     <div className="App">
       <Banner />
       <Navbar />
-      <Divider orientation='vertical' />
-      <SectionInfo />
+      
     </div>
   );
 }
--- src/Componenets/Banner.tsx
+++ src/Componenets/Banner.tsx
@@ -2,7 +2,7 @@ import { Box } from '@chakra-ui/react'
 
 const Banner: React.FC = () => {
     return (
-        <Box fontFamily={'Poppins'} bg='#3772FF' w='100%' p={4} color='white'>
+        <Box fontFamily={'Poppins'} bg='#3772FF' w='100%' h={36.48} lineHeight='0.4' fontWeight={600} fontStyle={'normal'} fontSize={14} p={4} color='#FFFFFF'>
             Lorem Ipsum is simply dummy text of the printing
         </Box>
     );
